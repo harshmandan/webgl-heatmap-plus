9e874f2fe6d54a0f720061008c73100fa9af6279
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-02-04 22:00:37 +0400

    add video player based on mux-inc/elements mux-video

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '@mux-elements/mux-video';
+
+	const video = {
+		src: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+		poster: 'https://image.mux.com/DZmOWg4VQcGbOgjWyU9zT01VauCJrAPYW/thumbnail.jpg'
+	};
+</script>
+
+<mux-video
+	src={video.src}
+	poster={video.poster}
+	controls
+	preload="metadata"
+	playsinline
+	stream-type="on-demand"
+	prefer-mse
+/>
+
+<style>
+	mux-video {
+		width: 100%;
+		height: 100%;
+		margin: 0 auto;
+	}
+</style>
